fe2e084ae3d34a20dff20d4c3fcdefb597428581
Ankita <Ankitashejal11@gmail.com>
2024-06-22 12:50:47 +0530

    change in email textfield

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -52,7 +52,7 @@ class LoginPageState extends State<LoginPage> {
                   child: Padding(
                     padding: const EdgeInsets.only(left: 20.0),
                     child: TextField(
-                      obscureText: true,
+                      
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         hintText: 'Email',
@@ -76,6 +76,7 @@ class LoginPageState extends State<LoginPage> {
                   child: Padding(
                     padding: const EdgeInsets.only(left: 20.0),
                     child: TextField(
+                      obscureText: true,
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         hintText: 'Password',
